5a840e3aa4e17986fee48064f71693601966f563
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2025-02-26 00:18:12 -0500

    use queue

--- test.cpp
+++ test.cpp
@@ -1,23 +1,20 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/pio.h"
-#include "hardware/interp.h"
 #include "hardware/timer.h"
 #include "hardware/watchdog.h"
+#include "pico/util/queue.h"
 
-#include "blink.pio.h"
-
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin)
-{
-    blink_program_init(pio, sm, offset, pin);
-    pio_sm_set_enabled(pio, sm, true);
-}
+queue_t input_queue;
 
 int64_t alarm_callback(alarm_id_t id, void *user_data)
 {
     static uint8_t cpt = 0;
-    gpio_put(PICO_DEFAULT_LED_PIN, cpt++ & 1);
-    return -10'000; // wait for another 500ms
+    if (cpt >= 2) // missing 1 tooth
+    {
+        gpio_put(PICO_DEFAULT_LED_PIN, cpt & 1);
+    }
+    cpt = (cpt + 1) % 48; // 24 teeth
+    return -10'000;       // wait for another 500ms
 }
 
 int main()
@@ -35,37 +32,39 @@ int main()
     // second arg is pause on debug which means the watchdog will pause when stepping through code
     watchdog_enable(100, 1);
 
-    // PIO Blinking example
-    PIO pio = pio0;
-    uint offset = pio_add_program(pio, &blink_program);
-    blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN);
-    // For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
-
-    // Timer example code - This example fires off the callback after 2000ms
-    add_alarm_in_ms(500, alarm_callback, NULL, false);
-    // For more examples of timer use see https://github.com/raspberrypi/pico-examples/tree/master/timer
+    {
+        queue_init(&input_queue, sizeof(uint32_t), 2);
+        gpio_irq_callback_t cb = [](uint, uint32_t)
+        {
+            const uint32_t timestamp = time_us_32();
+            queue_try_add(&input_queue, &timestamp);
+        };
+        gpio_set_irq_enabled_with_callback(PICO_DEFAULT_LED_PIN, GPIO_IRQ_EDGE_RISE, true, cb);
+    }
 
     // A device like Pico that uses a GPIO for the LED will define PICO_DEFAULT_LED_PIN
     // so we can use normal GPIO functionality to turn the led on and off
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
+    // Timer example code - This example fires off the callback after 2000ms
+    add_alarm_in_ms(500, alarm_callback, NULL, false);
+    // For more examples of timer use see https://github.com/raspberrypi/pico-examples/tree/master/timer
+
+    uint32_t last = 0;
+
     while (true)
     {
         // You need to call this function at least more often than the 100ms in the enable call to prevent a reboot
         watchdog_update();
 
-        if (!pio_sm_is_rx_fifo_empty(pio, 0)) {
-            printf("%d\n", pio_sm_get(pio, 0));
+        uint32_t now;
+        if (queue_try_remove(&input_queue, &now))
+        {
+            printf("%d\n", now - last);
+            last = now;
         }
+        tight_loop_contents();
+        sleep_ms(1);
     }
-
-    // Interpolator example code
-    interp_config cfg = interp_default_config();
-    // Now use the various interpolator library functions for your use case
-    // e.g. interp_config_clamp(&cfg, true);
-    //      interp_config_shift(&cfg, 2);
-    // Then set the config
-    interp_set_config(interp0, 0, &cfg);
-    // For examples of interpolator use see https://github.com/raspberrypi/pico-examples/tree/master/interp
 }
