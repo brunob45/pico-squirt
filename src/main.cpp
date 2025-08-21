22949a84386a2bd900edd883815c7fea81352a44
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2025-08-20 22:27:38 -0400

    testbench for flash write

--- src/main.cpp
+++ src/main.cpp
@@ -1,102 +1,119 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <tusb.h>
+
 #include "pico/stdlib.h"
-#include "hardware/watchdog.h"
+#include "pico/flash.h"
+#include "hardware/flash.h"
+#include "hardware/structs/systick.h"
 
-#include "canbus.h"
-#include "simulation.h"
-#include "decoder.h"
-#include "trigger.h"
+// We're going to erase and reprogram a region 256k from the start of flash.
+// Once done, we can access this at XIP_BASE + 256k.
+#define FLASH_TARGET_OFFSET (256 * 1024)
 
-static Decoder decoder;
-static Trigger trigger1, trigger2;
+const uint8_t *flash_target_contents = (const uint8_t *)(XIP_BASE + FLASH_TARGET_OFFSET);
+
+void print_buf(const uint8_t *buf, size_t len)
+{
+    for (size_t i = 0; i < len; ++i)
+    {
+        printf("%02x", buf[i]);
+        if (i % 16 == 15)
+            printf("\n");
+        else
+            printf(" ");
+    }
+}
+
+// This function will be called when it's safe to call flash_range_erase
+static void call_flash_range_erase(void *param)
+{
+    uint32_t offset = (uint32_t)param;
+    flash_range_erase(offset, FLASH_SECTOR_SIZE);
+}
+
+// This function will be called when it's safe to call flash_range_program
+static void call_flash_range_program(void *param)
+{
+    uint32_t offset = ((uintptr_t *)param)[0];
+    const uint8_t *data = (const uint8_t *)((uintptr_t *)param)[1];
+    flash_range_program(offset, data, FLASH_PAGE_SIZE);
+}
 
 int main()
 {
-    uint16_t target1, target2;
-
-    absolute_time_t last_print = get_absolute_time();
-
     stdio_init_all();
+    systick_hw->csr = 0x5;
+    systick_hw->rvr = 0x00FFFFFF;
 
-    // Watchdog example code - https://github.com/raspberrypi/pico-examples/tree/master/watchdog
-    if (watchdog_caused_reboot())
+    uint32_t begin, old, t0, t1;
+
+    while (!tud_cdc_connected())
     {
-        printf("Rebooted by Watchdog!\n");
+        sleep_ms(100);
     }
+    printf("tud_cdc_connected()\n");
 
-    // Enable the watchdog, requiring the watchdog to be updated every 100ms or the chip will reboot
-    // second arg is pause on debug which means the watchdog will pause when stepping through code
-    watchdog_enable(100, 1);
+    // t0 = time_us_32();
+    // old = systick_hw->cvr;
 
-    const uint LED1 = 16;
-    simulation_enable(LED1, 5000);
-    decoder.enable(LED1);
+    // sleep_us(50000);
+    // begin = systick_hw->cvr;
+    // t1 = time_us_32();
 
-    const uint LED2 = 17;
-    trigger1.init(LED2 + 0);
-    trigger2.init(LED2 + 1);
+    // uint32_t d = (old - begin) & 0x00FFFFFF;
 
+    // printf("\n          old-new=%ld\n", d);
+    // printf("            t1-t0=%ld\n", t1 - t0);
+    // printf("(old-new)/(t1-t0)=%.1f\n", (d) / (t1 - t0 * 1.0));
 
-    const uint LED3 = 19;
-    gpio_init(LED3);
-    gpio_set_dir(LED3, GPIO_OUT);
-    gpio_clr_mask(1 << LED3);
+    uint8_t random_data[FLASH_PAGE_SIZE];
+    for (uint i = 0; i < FLASH_PAGE_SIZE; ++i)
+        random_data[i] = rand() >> 16;
 
-    CANbus::setup();
+    printf("Generated random data:\n");
+    print_buf(random_data, FLASH_PAGE_SIZE);
 
-    uint event_triggered = 0;
-    uint cycle_time, cycle_max = 0;
-    uint last_cycle = time_us_32();
+    // Note that a whole number of sectors must be erased at a time.
+    printf("\nErasing target region...\n");
 
-    while (true)
+    // Flash is "execute in place" and so will be in use when any code that is stored in flash runs, e.g. an interrupt handler
+    // or code running on a different core.
+    // Calling flash_range_erase or flash_range_program at the same time as flash is running code would cause a crash.
+    // flash_safe_execute disables interrupts and tries to cooperate with the other core to ensure flash is not in use
+    // See the documentation for flash_safe_execute and its assumptions and limitations
+    old = time_us_32(); // systick_hw->cvr;
+    int rc = flash_safe_execute(call_flash_range_erase, (void *)FLASH_TARGET_OFFSET, UINT32_MAX);
+    begin = time_us_32(); // systick_hw->cvr;
+    hard_assert(rc == PICO_OK);
+
+    uint32_t d = (begin-old) & 0x00FFFFFF;
+    printf("Done in %ld. Read back target region:\n", d);
+    print_buf(flash_target_contents, FLASH_PAGE_SIZE);
+
+    printf("\nProgramming target region...\n");
+    uintptr_t params[] = {FLASH_TARGET_OFFSET, (uintptr_t)random_data};
+    old = time_us_32(); // systick_hw->cvr;
+    rc = flash_safe_execute(call_flash_range_program, params, UINT32_MAX);
+    begin = time_us_32(); // systick_hw->cvr;
+    hard_assert(rc == PICO_OK);
+    d = (begin-old) & 0x00FFFFFF;
+    printf("Done in %ld. Read back target region:\n", d);
+    print_buf(flash_target_contents, FLASH_PAGE_SIZE);
+
+    bool mismatch = false;
+    for (uint i = 0; i < FLASH_PAGE_SIZE; ++i)
     {
-        gpio_xor_mask(1 << LED3);
-
-        // You need to call this function at least more often than the 100ms in the enable call to prevent a reboot
-        watchdog_update();
-        simulation_update();
-
-        if (decoder.update())
-        {
-            if (decoder.sync_count == 1)
-            {
-                if (event_triggered == 0)
-                {
-                    printf("missed event\n");
-                }
-                else if (event_triggered > 1)
-                {
-                    printf("multiple events %d\n", event_triggered);
-                }
-                event_triggered = 0;
-            }
-        }
-
-        if (decoder.compute_target(&trigger1, target1, 3000))
-        {
-            event_triggered += 1;
-            target1 += 91;
-        }
-        if (decoder.compute_target(&trigger2, target2, 3000))
-        {
-            target2 -= 91;
-        }
-
-        const absolute_time_t this_cycle = get_absolute_time();
-        cycle_time = this_cycle - last_cycle;
-        last_cycle = this_cycle;
-        if (cycle_time > cycle_max)
-            cycle_max = cycle_time;
-
-        if (absolute_time_diff_us(last_print, this_cycle) >= 1e6)
-        {
-            last_print = this_cycle;
-            CANbus::transmit(1 | 1<<31, 2, cycle_max, cycle_max>>8, 0, 0, 0, 0, 0, 0);
-            // printf("%d,", cycle_max);
-            // trigger1.print_debug();
-            // trigger2.print_debug();
-            // printf("\n");
-            cycle_max = 0;
-        }
+        if (random_data[i] != flash_target_contents[i])
+            mismatch = true;
     }
+    if (mismatch)
+        printf("Programming failed!\n");
+    else
+        printf("Programming successful!\n");
+
+    while (1)
+        tight_loop_contents();
+
+    return 0;
 }
