1f8aed5750b0ff2ec5062bf6778212b46048e628
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2025-08-23 18:41:59 -0400

    create twi logic

--- avr/main.cpp
+++ avr/main.cpp
@@ -8,16 +8,28 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+void twi_init();
+
+void set_out(bool out) {
+    PORTC.DIRSET = (1<<0);
+    if (out)
+        PORTC.OUTSET = (1<<0);
+    else
+        PORTC.OUTCLR = (1<<0);
+}
 
 int main(void)
 {
-    PORTA.DIR = 1;
+    ADC0.CTRLA = (1 << ADC_ENABLE_bp);
+    twi_init();
+
+    set_out(0);
 
     while (1) 
     {
-        PORTA.OUT = 1;
+        set_out(1);
         _delay_ms(500);
-        PORTA.OUT = 0;
+        set_out(0);
         _delay_ms(500);
     }
 }
