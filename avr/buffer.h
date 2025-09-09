4273f3928f35c4b3abae6c5ce30a4212de3cf6ee
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2025-09-09 00:31:57 -0400

    fix buffer overflow

--- avr/buffer.h
+++ avr/buffer.h
@@ -2,7 +2,7 @@
 
 class Buffer
 {
-    uint8_t buf[8];
+    uint8_t buf[16];
     uint8_t head, tail, full;
 
 public:
@@ -13,6 +13,8 @@ public:
         {
             buf[head] = value;
             head += 1;
+            if (head >= sizeof(buf))
+                head = 0;
             full = (head == tail);
             ret = true;
         }
@@ -26,6 +28,8 @@ public:
         {
             value = buf[tail];
             tail += 1;
+            if (tail >= sizeof(buf))
+                tail = 0;
             full = 0;
         }
         return value;
