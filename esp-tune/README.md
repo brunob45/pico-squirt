f0bb23da80296fc5e49542d547801e713bfb270c
Bruno Bousquet <bruno.bousquet@tld-america.com>
2025-09-16 18:00:52 -0400

    add readme and create AP

---
+++ esp-tune/README.md
@@ -0,0 +1,7 @@
+# ESP-Tune
+
+Front-end for DIY ECU
+
+## Dependencies
+- [ESPAsyncWebServer](https://github.com/ESP32Async/ESPAsyncWebServer.git)
+- [AsyncTCP](https://github.com/ESP32Async/AsyncTCP)
--- esp-tune/esp-tune.ino
+++ esp-tune/esp-tune.ino
@@ -1,22 +1,20 @@
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
-#include <LittleFS.h> // use LittleFS instead of SPIFFS
+#include <LittleFS.h>  // use LittleFS instead of SPIFFS
 
-const char *ssid = "YOUR_WIFI_SSID";
-const char *password = "YOUR_WIFI_PASSWORD";
+const char *apSSID = "GaugeServer";
+const char *apPASS = "12345678";  // at least 8 chars
 
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
 
-float values[8] = {10, 20, 30, 40, 50, 60, 70, 80};
+float values[8] = { 10, 20, 30, 40, 50, 60, 70, 80 };
 unsigned long lastSend = 0;
 
 // Send JSON with 8 values to all clients
-void notifyClients()
-{
+void notifyClients() {
   String json = "{\"v\":[";
-  for (int i = 0; i < 8; i++)
-  {
+  for (int i = 0; i < 8; i++) {
     json += String(values[i], 1);
     if (i < 7)
       json += ",";
@@ -25,55 +23,53 @@ void notifyClients()
   ws.textAll(json);
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
 
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nWiFi connected! IP: " + WiFi.localIP().toString());
+  // --- Start AP mode ---
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(apSSID, apPASS);
+  Serial.println("AP started");
+  Serial.print("SSID: ");
+  Serial.println(apSSID);
+  Serial.print("Password: ");
+  Serial.println(apPASS);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.softAPIP());
 
   // --- Mount LittleFS ---
-  if (!LittleFS.begin(true))
-  {
+  if (!LittleFS.begin(true)) {
     Serial.println("LittleFS Mount Failed!");
     return;
   }
 
   // Serve the index page from LittleFS
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(LittleFS, "/index.html", "text/html"); });
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(LittleFS, "/index.html", "text/html");
+  });
 
   // Optionally serve any other static files (css, js, etc.)
   server.serveStatic("/", LittleFS, "/");
 
   // WebSocket events
   ws.onEvent([](AsyncWebSocket *s, AsyncWebSocketClient *c,
-                AwsEventType t, void *arg, uint8_t *data, size_t len)
-             {
+                AwsEventType t, void *arg, uint8_t *data, size_t len) {
     if (t == WS_EVT_CONNECT) {
       Serial.printf("WS client #%u connected\n", c->id());
-    } });
+    }
+  });
 
   server.addHandler(&ws);
   server.begin();
 }
 
-void loop()
-{
+void loop() {
   ws.cleanupClients();
 
   // Simulate sensor data & push every 500 ms
-  if (millis() - lastSend > 500)
-  {
+  if (millis() - lastSend > 500) {
     lastSend = millis();
-    for (int i = 0; i < 8; i++)
-    {
+    for (int i = 0; i < 8; i++) {
       values[i] += random(-10, 10) * 0.1;
       if (values[i] < 0)
         values[i] = 0;
