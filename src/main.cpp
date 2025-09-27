70f8211e92d1537e2d81f76f9cdc0eb9710316df
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2025-09-27 01:27:57 -0400

    begin decoding of serial protocol

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,14 @@
 #include "hardware/gpio.h"
 #include "hardware/adc.h"
 
+#include "tusb.h"
+
 #include "avr.h"
 #include "decoder.h"
 #include "flash.h"
 #include "global_state.h"
 #include "linear_interp.h"
+#include "crc32.h"
 
 static GlobalState gs;
 
@@ -32,24 +35,98 @@ static struct
     int16_t ve_y_axis[16];
 } page1;
 
+void transmit_response(const uint8_t *buffer, size_t n)
+{
+    // compute crc on payload (exclude size)
+    const uint32_t crc = Crc32_ComputeBuf(0, buffer, n);
+    // final packet size
+    const uint16_t packet_size = n + 6;
+
+    if (true)
+    {
+        // transmit size first
+        tud_cdc_write_char(packet_size >> 8);
+        tud_cdc_write_char(packet_size);
+        // then the actual payload
+        tud_cdc_write(buffer, n);
+        // and finally the crc
+        tud_cdc_write_char(crc >> 24);
+        tud_cdc_write_char(crc >> 16);
+        tud_cdc_write_char(crc >> 8);
+        tud_cdc_write_char(crc >> 0);
+    }
+    else
+    {
+        // debug
+        printf("%d:", packet_size);
+        for (size_t i = 0; i < n; i++)
+        {
+            printf("%x,", buffer[i]);
+        }
+        printf("%x\n", crc);
+    }
+}
+
 void core1_entry()
 {
-    auto last_trx = get_absolute_time();
+    static uint8_t buffer[4096];
+    static uint16_t buffer_size = 0;
+
     while (true)
     {
-        if (get_absolute_time() - last_trx > 1'000'000) // 1s
+        buffer_size += tud_cdc_read(buffer + buffer_size, sizeof(buffer) - buffer_size);
+        if (buffer_size >= 7) // minimum size for valid packet
         {
-            last_trx = get_absolute_time();
+            uint16_t packet_size = buffer[1] + 0x100 * buffer[0]; // big-endian
+            printf("%d\n", packet_size);
+            if (buffer_size >= packet_size) // full packet received
+            {
+                gpio_xor_mask(1 << 25);
+                uint32_t crc_rx = buffer[buffer_size - 1] +
+                                  (0x100 * buffer[buffer_size - 2]) +
+                                  (0x10000 * buffer[buffer_size - 3]) +
+                                  (0x1000000 * buffer[buffer_size - 4]);
+                uint32_t crc = Crc32_ComputeBuf(0, buffer + 2, buffer_size - 6); // exclude size and crc
 
-            // Print ADC values
-            printf("ADC0: %0.2f, %0.1f, ", 5.0f / 0x3fff * gs.adc[0], gs.manifold_pressure * 0.1f);
-            printf("ADC1: %0.2f, %0.1f, ", 5.0f / 0x3fff * gs.adc[1], gs.manifold_temperature * 0.1f);
-            printf("ADC2: %0.2f, %0.1f, ", 5.0f / 0x3fff * gs.adc[2], gs.coolant_temperature * 0.1f);
-            printf("ADC3: %0.2f, %0.1f, ", 5.0f / 0x3fff * gs.adc[3], gs.throttle_position * 0.1f);
-            printf("ADC4: %0.2f, %0.1f, ", 5.0f / 0x3fff * gs.adc[4], gs.battery_voltage * 0.01f);
-            printf("ADC5: %0.2f, %0.1f, ", 5.0f / 0x3fff * gs.adc[5], gs.air_fuel_ratio * 0.01f);
-            printf("ADC6: %0.2f, %0.1f, ", 5.0f / 0x3fff * gs.adc[6], 0.0f);
-            printf("%0.1f°C, %d, %d\n", gs.pico_temperature * 0.1f, gs.loop_time_avg, gs.loop_time_max);
+                // printf("%x==%x\n", crc_rx, crc);
+
+                switch (buffer[2])
+                {
+                case 'c':
+                {
+                    uint16_t seconds = time_us_64() / 1'000'000;
+                    uint8_t res[] = {
+                        0, // OK flag
+                        (uint8_t)(seconds >> 8),
+                        (uint8_t)(seconds),
+                    };
+                    transmit_response(res, sizeof(res));
+                    break;
+                }
+                case 'F':
+                {
+                    uint8_t res[] = " 002";
+                    res[0] = 0;                              // OK flag
+                    transmit_response(res, sizeof(res) - 1); // ignore last null char
+                    break;
+                }
+                case 'Q':
+                {
+                    uint8_t res[] = " MS2Extra comms342h2";
+                    res[0] = 0;                              // OK flag
+                    transmit_response(res, sizeof(res));
+                    break;
+                }
+                case 'S':
+                {
+                    uint8_t res[] = " Picosquirt 2025-09";
+                    res[0] = 0;                              // OK flag
+                    transmit_response(res, sizeof(res));
+                    break;
+                }
+                }
+                buffer_size = 0;
+            }
         }
     }
 }
@@ -140,7 +217,7 @@ int main()
                 // printf("x:%d y:%d z:%d\n", x, y, z);
                 cpt++;
             }
-            printf("%ld\n", cpt);
+            // printf("%ld\n", cpt);
         }
 
         // Compute loop time
